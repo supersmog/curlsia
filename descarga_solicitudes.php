44b60a8d62c22ddcd3dd7cfc63338ecdfcc6dd22
Miguel Escalante <angelescalante@gmail.com>
2019-06-25 22:24:26 -0500

    version analiza solicitudes

--- descarga_solicitudes.php
+++ descarga_solicitudes.php
@@ -36,7 +36,7 @@ function analiza_archivo_solicitud($file)
         $aDataTableHeaderHTML[]=trim($NodeHeader->textContent);
        //echo $aDataTableHeaderHTML;
     }
-     print_r($aDataTableHeaderHTML);
+     //print_r($aDataTableHeaderHTML);
      
      
      $fecha_presupuesto=substr($aDataTableHeaderHTML[12],23,10);
@@ -48,68 +48,80 @@ function analiza_archivo_solicitud($file)
      $tel_casa_cliente=$aDataTableHeaderHTML[45];
      $tel_ofi_cliente=$aDataTableHeaderHTML[47];
      $tel_cel_cliente=$aDataTableHeaderHTML[49];
-
      $empresa_cliente=$aDataTableHeaderHTML[76];
      $puesto_empresa_cliente=$aDataTableHeaderHTML[78];
      $direccion_empresa_cliente=$aDataTableHeaderHTML[80];
+     $telefono_empresa_cliente=$aDataTableHeaderHTML[82];
+     $nombre_referencia1=$aDataTableHeaderHTML[86];
+     $parentesco_referencia1=$aDataTableHeaderHTML[88];
+     $direccion_referencia1=$aDataTableHeaderHTML[90];
+     $telefono_referencia1=$aDataTableHeaderHTML[92];
+     $nombre_referencia2=$aDataTableHeaderHTML[96];
+     $parentesco_referencia2=$aDataTableHeaderHTML[98];
+     $direccion_referencia2=$aDataTableHeaderHTML[100];
+     $telefono_referencia2=$aDataTableHeaderHTML[102];
+     $nombre_aval=$aDataTableHeaderHTML[122];
+     $rfc_aval=$aDataTableHeaderHTML[124];
+     $curp_aval=$aDataTableHeaderHTML[126];
+     $direccion_aval=$aDataTableHeaderHTML[128];
+     $colonia_aval=$aDataTableHeaderHTML[130];
+     $telefono_aval=$aDataTableHeaderHTML[132];
+
+     echo "###Datos Cliente##\n";
+     echo "$nombre_cliente\n";
+     echo "$rfc_cliente\n";
+     echo "$curp_cliente\n";
+     echo "$dirección_cliente\n";
+     echo "$colonia_cliente\n";
+     echo "$tel_casa_cliente\n";
+     echo "$tel_ofi_cliente\n";
+     echo "$tel_cel_cliente\n";
+     echo "###Trabajo Cliente##\n";
+     echo "$empresa_cliente\n";
+     echo "$puesto_empresa_cliente\n";
+     echo "$direccion_empresa_cliente\n";
+     echo "$telefono_empresa_cliente\n";
+     echo "###Referencia1 Cliente##\n";
+     echo "$nombre_referencia1\n";
+     echo "$parentesco_referencia1\n";
+     echo "$direccion_referencia1\n";
+     echo "$telefono_referencia1\n";
+     echo "###Referencia2 Cliente##\n";
+     echo "$nombre_referencia2\n";
+     echo "$parentesco_referencia2\n";
+     echo "$direccion_referencia2\n";
+     echo "$telefono_referencia2\n";
+     echo "###Aval Cliente##\n";
+     echo "$nombre_aval\n";
+     echo "$rfc_aval\n";
+     echo "$curp_aval\n";
+     echo "$direccion_aval\n";
+     echo "$colonia_aval\n";
+     echo "$telefono_aval\n";
 
 
 
-     $rpu=$aDataTableHeaderHTML[19];
-     $telefono=$aDataTableHeaderHTML[21];
-     $presupuesto=$aDataTableHeaderHTML[23];
-     $marca_instalar=$aDataTableHeaderHTML[31];
-     $modelo_instalar=$aDataTableHeaderHTML[32];
-     $capacidad_instalar=$aDataTableHeaderHTML[33];
-     $monto_financiar=$aDataTableHeaderHTML[34];
-     $marca_retirar=$aDataTableHeaderHTML[42];
-     $capacidad_retirar=$aDataTableHeaderHTML[43];
-     $modelo_retirar=$aDataTableHeaderHTML[44];
-     $solicitud=$aDataTableHeaderHTML[47];
-     $precio_sin_iva=$aDataTableHeaderHTML[51];
-     $iva=ltrim($aDataTableHeaderHTML[54]);
-     $excedente=$aDataTableHeaderHTML[63];
-     $interes=$aDataTableHeaderHTML[69];
-     $iva_interes=$aDataTableHeaderHTML[72];
-     $monto_financiar2=$aDataTableHeaderHTML[66];
-     $financiado=$aDataTableHeaderHTML[75];
-     $amortizacion=$aDataTableHeaderHTML[78];
-     $num_pagos=substr($aDataTableHeaderHTML[76],0,2);
-
-
-     $fecha_presupuesto=obtiene_fecha($fecha_presupuesto);
-     $precio_sin_iva=substr($precio_sin_iva,1,9);
-     $iva=substr($iva,17,8);
-     $monto_financiar=substr($monto_financiar2,16,10);
-     $excedente=substr($excedente,16,7);
-     $interes=substr($interes,17,8);
-     $iva_interes=substr($iva_interes,16,7);
-     
-     $financiado=substr($financiado,17,9);
-     $amortizacion=substr($amortizacion,17,6);
-
-
-     $sp="RF";
-     $activo=1;
-     $sql="INSERT INTO
-     presupuestos (fecha,nombre,rpu,num_presupuesto,telefono,marca_ins,modelo_ins,capacidad_ins,
-       marca_ret,modelo_ret,capacidad_ret,solicitud,instalacion,precio_sin_iva,iva_equipo,monto_financiar,
-       excedente,interes,iva_interes,total_financiamiento,amortizacion,pagos,subprograma,activo)
-     VALUES (
-       '$fecha_presupuesto', '$nombre_cliente','$rpu','$presupuesto','$telefono','$marca_instalar',
-       '$modelo_instalar','$capacidad_instalar','$marca_retirar','$modelo_retirar','$capacidad_retirar',
-       '$solicitud','0','$precio_sin_iva','$iva','$monto_financiar','$excedente','$interes',
-       '$iva_interes','$financiado','$amortizacion','$num_pagos','$sp','$activo'
-     )";
-        $resp=$cliente->insertar($sql);
-        if($resp)
-        {
-            echo "Se guardo correctamente";
-        }
-        else{
-            echo "No se pudo guardar";
-        }
-echo $sql;
+//     $sp="RF";
+//     $activo=1;
+//     $sql="INSERT INTO
+//     presupuestos (fecha,nombre,rpu,num_presupuesto,telefono,marca_ins,modelo_ins,capacidad_ins,
+//       marca_ret,modelo_ret,capacidad_ret,solicitud,instalacion,precio_sin_iva,iva_equipo,monto_financiar,
+//       excedente,interes,iva_interes,total_financiamiento,amortizacion,pagos,subprograma,activo)
+//     VALUES (
+//       '$fecha_presupuesto', '$nombre_cliente','$rpu','$presupuesto','$telefono','$marca_instalar',
+//       '$modelo_instalar','$capacidad_instalar','$marca_retirar','$modelo_retirar','$capacidad_retirar',
+//       '$solicitud','0','$precio_sin_iva','$iva','$monto_financiar','$excedente','$interes',
+//       '$iva_interes','$financiado','$amortizacion','$num_pagos','$sp','$activo'
+//     )";
+//        $resp=$cliente->insertar($sql);
+//        if($resp)
+//        {
+//            echo "Se guardo correctamente";
+//        }
+//        else{
+//            echo "No se pudo guardar";
+//        }
+//echo $sql;
 }
 
  /* function descarga_presupuestos()
@@ -153,23 +165,10 @@ $login="http://www.programaasibc.com.mx/siaMexicali/validausuario.php";
 $data_login="usuario=md032ca&clave=md032ca&tipo=1";
 //
 
-descarga_archivo_sindata($login,$data_login,"http://www.programaasibc.com.mx/siaMexicali/contratos/ImpresionSol.php?tabla=1&fecha_supervision=&solicitud=YU000015&solixtra=2","paginas/solicitud_yu000015-2.html");
-analiza_archivo_solicitud("paginas/solicitud_yu000015-2.html");
-
-//descarga_archivo_sindata($login,$data_login,"http://www.programaasibc.com.mx/siaMexicali/consolusu.php?fi=2019-05-01&ff=2019-05-31&tip=men&en=5","paginas/colocadas_mayo.html");
-//descarga_archivo_sindata($login,$data_login,"http://www.programaasibc.com.mx/siaMexicali/consolusu.php?fi=2019-04-01&ff=2019-04-31&tip=men&en=4","paginas/colocadas_abril.html");
-//descarga_archivo_sindata($login,$data_login,"http://www.programaasibc.com.mx/siaMexicali/consolusu.php?fi=2019-03-01&ff=2019-03-31&tip=men&en=3","paginas/colocadas_marzo.html");
-//descarga_archivo_sindata($login,$data_login,"http://www.programaasibc.com.mx/siaMexicali/consolusu.php?fi=2019-06-01&ff=2019-06-31&tip=men&en=6","paginas/colocadas_junio.html");
-
-
-//Descargar liberaciones de diversos reportes
-
-//Liberaciones reporte contabilidad 
-//descarga_archivo_condata($login,$data_login,"http://www.programaasibc.com.mx/siaMexicali/liberacion/reportes/liberaciones_Conta.php","Cual=-1&idCoordinacion=-1&idDistribuidor=-1&pdto=10&orderby=1&fechaInicial=2018-09-01&model=1&xml=0&fechaFinal=2019-12-31","paginas/liberacionesconta.html");
-/*
-//Liberaciones reporte tecnico 
-*/
-//descarga_archivo_condata($login,$data_login,"http://www.programaasibc.com.mx/siaMexicali/liberacion/reportes/liberaciones2.php","Cual=&idCoordinacion=-1&equipo=-1&idDistribuidor=-1&pdto=10&orderby=1&fechaLib=2018-09-01&model=1&xml=1&fechaInicial=2018-09-01&fechaFinal=2019-12-31&dAtencion=1","paginas/liberaciones_tec.html");
+descarga_archivo_sindata($login,$data_login,"http://www.programaasibc.com.mx/siaMexicali/contratos/ImpresionSol.php?tabla=1&fecha_supervision=&solicitud=YU001014&solixtra=1","paginas/solicitud_YU001014-1.html");
+descarga_archivo_sindata($login,$data_login,"http://www.programaasibc.com.mx/siaMexicali/contratos/ImpresionSol.php?tabla=1&fecha_supervision=&solicitud=YU001013&solixtra=1","paginas/solicitud_YU001012-1.html");
+analiza_archivo_solicitud("paginas/solicitud_YU001014-1.html");
+analiza_archivo_solicitud("paginas/solicitud_YU00101-1.html");
 
 
 
